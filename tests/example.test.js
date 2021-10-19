ddb059a5e9fd823ebf4093d3842d51c791ef194e
EmmanuelQAstaneda <eliascastaneda.im@gmail.com>
2021-10-19 11:38:04 -0500

    Código del desafio numero 3 con Aserciones

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,39 +1,32 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
-describe('Desafio numero 2 usando puppeteer.', () => {
+describe('Desafio numero 3: Usando Aserttions.', () => {
 
-    it('Click al boton \"Women\" utilizando el texto del boton.', async () => {
-        const browser = await puppeteer.launch({ headless: false })
-        const page = await browser.newPage()
+    let browser, page
+    var product = 5
+
+    before(async()=>{
+        browser = await puppeteer.launch({ headless: false })
+        page = await browser.newPage()
         await page.goto('http://automationpractice.com/index.php')
-        await page.waitForSelector('head')
-        const button = await page.$x('//*[contains(text(),\'Women\')]')
-        button[0].click()
-        await page.waitForXPath('//*[contains(text(),\'This category includes all the basics of your wardrobe and much more:\')]')
+    })
 
+    after(async()=>{
         await browser.close()
     })
 
-    it('Click al boton \"Women\" utilizando el xpath del boton.', async () => {
-        const browser = await puppeteer.launch({ headless: false })
-        const page = await browser.newPage()
-        await page.goto('http://automationpractice.com/index.php')
-        await page.waitForSelector('head')
-        const button = await page.$x('//a [@title=\'Women\']')
-        button[0].click()
-        await page.waitForXPath('//*[contains(text(),\'This category includes all the basics of your wardrobe and much more:\')]')
-
-        await browser.close()
-    })
-
-    it('Click al boton \"Women\" utilizando el css selector del boton.', async () => {
-        const browser = await puppeteer.launch({ headless: false })
-        const page = await browser.newPage()
-        await page.goto('http://automationpractice.com/index.php')
-        await page.waitForSelector('head')
-        await page.click('a[title=\"Women\"]',)
-        await page.waitForXPath('//*[contains(text(),\'This category includes all the basics of your wardrobe and much more:\')]')
-
-        await browser.close()
+    it('Revisar los elementos de la tienda.', async () => {
+                
+        const price = await page.$$eval('#homefeatured >li > div > div[class="right-block"] > div > span[itemprop="price"]',(options) => options.map((option) => option.textContent))
+        await page.click('[data-id-product="'+product+'"]')
+        await page.waitForSelector('span[title="Close window"]', {visible : true})
+        await page.click('span[title="Close window"]')
+        await page.click('a[title="View my shopping cart"]')
+        await page.waitForSelector('td[data-title="Unit price"] > span > span',{visible : true})
+        const priceCar = await page.$eval('td[data-title="Unit price"] > span > span',(option) => option.textContent) 
+        await expect(price[product-1]).to.include(priceCar)
+                
     })
 })
+  
